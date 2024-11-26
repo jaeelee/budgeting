ce3a80396e599ee21eb83c045903a2582e3993bb
jaeelee <jaeelee.dev@gmail.com>
2024-11-26 16:49:57 +0900

    Initial Commit

---
+++ ShrimpSlide/ShrimpSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrimpSlideApp.swift
+//  ShrimpSlide
+//
+//  Created by jaeeun on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ShrimpSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShrimpSlideTests/ShrimpSlideTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShrimpSlideTests.swift
+//  ShrimpSlideTests
+//
+//  Created by jaeeun on 11/26/24.
+//
+
+import Testing
+@testable import ShrimpSlide
+
+struct ShrimpSlideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
